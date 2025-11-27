17e714ccb9c93145f3c17edd5393aa1698116330
Tushika21 <155798759+Tushika21@users.noreply.github.com>
2025-11-27 12:27:21 +0530

    Refactor outputs in outputs.tf for EKS module
    Removed old output variables and added new outputs for cluster name and kubeconfig.

--- infrastructure/outputs.tf
+++ infrastructure/outputs.tf
@@ -1,19 +1,8 @@
-output "ec2_public_ip" {
-  description = "Public IP address of the EC2 instance"
-  value       = aws_instance.brb_app.public_ip
+output "cluster_name" {
+  value = module.eks.cluster_name
 }
 
-output "application_url" {
-  description = "URL where the application will be available"
-  value       = "https://brb.elvisquant.com"
-}
-
-output "s3_bucket_name" {
-  description = "Name of the S3 bucket for Terraform state"
-  value       = data.aws_s3_bucket.tf_state.bucket
-}
-
-output "dynamodb_table" {
-  description = "Name of the DynamoDB table for state locking"
-  value       = data.aws_dynamodb_table.tf_locks.name
+output "kubeconfig" {
+  value = module.eks.kubeconfig
+  sensitive = true
 }
